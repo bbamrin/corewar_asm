ae1b7bda0dbb11a5b6fa0ccecb6b69eb87bc524e
bbamrin <bbamrin@gmail.com>
2020-11-12 11:15:23 +0300

    init

---
+++ vm-champ-linux/zork.s
@@ -0,0 +1,36 @@
+.name "zork"
+.comment "I'M ALIIIIVE"
+
+l10:
+l9:
+l8:
+
+
+l7:
+
+
+
+
+
+l6:
+
+
+
+
+
+
+
+
+
+l2:		sti r1, %:live, %1
+		and r1, %0, r1
+
+kek:
+kek1:
+kek1:
+live:	live %1
+		zjmp %:live
+
+live:	live %1
+		zjmp %10
+
